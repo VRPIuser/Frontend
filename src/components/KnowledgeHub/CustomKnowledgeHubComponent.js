a3a465f33af4ca8a6c41570a0054871f9428a6f3
pavan <pavan49450@gmail.com>
2024-04-10 14:18:59 +0530

    Changed the navigation for register.

--- src/components/KnowledgeHub/CustomKnowledgeHubComponent.js
+++ src/components/KnowledgeHub/CustomKnowledgeHubComponent.js
@@ -5,9 +5,7 @@ import Section from "../../UI/Sections/Section";
 import { useEffect, useState } from "react";
 import HighlightCapsWords from "../../UI/HighlightWords/HighlightCapsWords";
 import { useDispatch } from "react-redux";
-import { setComingSoon } from "../../store/ComingSoonSlice";
-import InternshipCard from "./InternshipCard/InternshipCard";
-import CourseCard from "./CourseCard/CourseCard";
+import { useNavigate } from "react-router-dom";
 import OurPartners from "../OurPartners/OurPartners";
 import AllCardsSection from "./AllCardsSection/AllCardsSection";
 
@@ -21,63 +19,14 @@ const CustomKnowledgeHubComponent = ({ data, backgroundImage }) => {
     setWidth(window.innerWidth);
   };
 
-  const onlineCourses =
-    data.Courses && data.Courses.filter((course) => course.type === "online");
-
-  const offlineCourses =
-    data.Courses && data.Courses.filter((course) => course.type === "offline");
-  const upcomingCourses =
-    data.Courses && data.Courses.filter((course) => course.type === "upcoming");
-
-  const allCourses = [
-    {
-      title: "Edu-Tech Online Courses",
-      highlightWord: "Edu-Tech",
-      courses: onlineCourses,
-    },
-    {
-      title: "Edu-Tech Offline Courses",
-      highlightWord: "Edu-Tech",
-      courses: offlineCourses,
-    },
-    {
-      title: "Upcoming Courses",
-      highlightWord: "Upcoming",
-      courses: upcomingCourses,
-    },
-  ];
-
-  const liveInternships =
-    data.Internships &&
-    data.Internships.filter((intership) => intership.type === "live");
-  const upcomingInternships =
-    data.Internships &&
-    data.Internships.filter((intership) => intership.type === "upcoming");
-
-  const allInternships = [
-    {
-      title: "Internships on",
-      highlightWord: "Internships",
-      internships: liveInternships,
-    },
-    {
-      title: "Upcoming Internships",
-      highlightWord: "Internships",
-      internships: upcomingInternships,
-    },
-  ];
-
   useEffect(() => {
-    // console.log(onlineCourses);
-    // console.log(offlineCourses);
-    // console.log(upcomingCourses);
-    // console.log("L IN", liveInternships);
-    // console.log("Up IN", upcomingInternships);
     window.addEventListener("resize", handleResize);
     return () => {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  const navigate = useNavigate();
 
   return (
     <div style={{ width: "100%" }}>
@@ -112,10 +61,7 @@ const CustomKnowledgeHubComponent = ({ data, backgroundImage }) => {
                 {data.mainContent.button.name}
               </NavLink>
             ) : (
-              <button
-                className={style.btn}
-                onClick={() => dispatch(setComingSoon(true))}
-              >
+              <button className={style.btn} onClick={() => navigate("/signup")}>
                 {" "}
                 {data.mainContent.button.name}
               </button>
