f9f2b2c0dc118ea80427ed9602c4c5a972717d59
VRPIuser <166590202+VRPIuser@users.noreply.github.com>
2024-04-19 15:33:02 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const url = {
   // backendBaseUrl: "https://vrpitest.azurewebsites.net",
   // backendBaseUrl: "http://localhost:8080",
-  backendBaseUrl: "http://35.153.51.171:8080",
+  backendBaseUrl: "http://44.212.253.73:8080",
   // backendBaseUrl: "https://vrpi-usermodule.azurewebsites.net",
   termsAndConditions:
     "https://merchant.razorpay.com/policy/NaFsnQOzqL4ESZ/terms",
