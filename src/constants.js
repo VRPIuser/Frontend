d3f69325ee886def3318773e3dd108330fc2d718
pavan <pavan49450@gmail.com>
2024-05-02 09:46:12 +0530

    Changed the base URL

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 export const url = {
+  backendBaseUrl: "http://vrpigroup.com",
+
   // backendBaseUrl: "https://vrpitest.azurewebsites.net",
   // backendBaseUrl: "http://localhost:8080",
-  backendBaseUrl: "http://44.212.253.73:8080",
+  // backendBaseUrl: "http://44.212.253.73:8080",
   // backendBaseUrl: "https://vrpi-usermodule.azurewebsites.net",
   termsAndConditions:
     "https://merchant.razorpay.com/policy/NaFsnQOzqL4ESZ/terms",
